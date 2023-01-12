59833545122adb5ebd2a6a1d378567896d8466de
venu <venugottemukkula@gmail.com>
2023-01-12 12:01:25 +0530

    hello world project creatd

---
+++ helloworld/src/main/java/com/staragile/GitAssignment.java
@@ -0,0 +1,12 @@
+package com.staragile;
+
+public class GitAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		System.out.println("Hi");
+		System.out.println("Hello");
+		System.out.println("good morning");
+	}
+
+}
